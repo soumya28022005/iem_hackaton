201c53cae9549f0f80dc37c8121c818f72738b5e
Your Name <you@example.com>
2026-05-01 21:28:18 +0530

    chore: cleanup unwanted and unused files/folders

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  transpilePackages: ['framer-motion'],
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
 };
